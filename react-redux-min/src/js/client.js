1f38d16d245f92359edea705fae5e3285ba148b5
muruso <t.hiatus-s.ryo@docomo.ne.jp>
2019-07-06 15:19:34 +0900

    simple increment

--- react-redux-min/src/js/client.js
+++ react-redux-min/src/js/client.js
@@ -1,1 +1,29 @@
-// import { createStore } from "redux";
+import { createStore } from "redux";
+
+// const reducer = () => {
+//   console.log("reducer has been called.");
+// }
+
+const reducer = (state = 0, action) => {
+  switch(action.type) {
+    case "INC":
+      return state + action.payload;
+    case "DEC":
+      return state - action.payload;
+  }
+  return state;
+}
+
+const store = createStore(reducer, 10000);
+
+store.subscribe(() => {
+  console.log("store changed", store.getState());
+});
+
+store.dispatch({type: "INC", payload: 12});
+store.dispatch({type: "INC", payload: 134});
+store.dispatch({type: "INC", payload: 3});
+store.dispatch({type: "INC", payload: 3});
+store.dispatch({type: "DEC", payload: 333333333});
+
+
